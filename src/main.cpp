dde13a64e33561738ab3e795c205ee6b7f082762
yxsong <yxsong@gmail.com>
2025-04-11 21:18:42 -0400

    Make it run

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <MeMCore.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Define motors
+MeDCMotor motorLeft(M1);
+MeDCMotor motorRight(M2);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Nothing required
 }
+
+int speed = 150;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Spin both motors forward at 100% speed
+  motorLeft.run(speed);    // range: -255 to 255
+  motorRight.run(speed);   // negative = reverse
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(2000);           // run for 2 seconds
+
+  // Stop the motors
+  motorLeft.run(0);
+  motorRight.run(0);
+
+  delay(2000);           // wait 2 seconds
+
+  speed = -speed;
 }
